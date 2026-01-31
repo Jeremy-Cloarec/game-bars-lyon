8b0fd5fd2ed7376d596698772713c1641af6e8af
Jerem <jeremycloarec@msn.com>
2026-01-31 21:01:23 +0100

    fix: change url namein .env

--- app/index.ts
+++ app/index.ts
@@ -10,5 +10,5 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server running at port ${port}`);
+    console.log(`Server running at port ${process.env.API_URL}`);
 })
